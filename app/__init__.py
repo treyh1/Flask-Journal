d6055228a0669bcf8ff4fafa6296410c117289b9
TH <thow123@gmail.com>
2018-03-10 14:42:38 -0800

    app:app
    change to procfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,9 @@
 from flask import Flask
-from config import Config 
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
 from app import models, routes
-
-if __name__ == '__main__':
-   app.run(debug = True, use_reloader=True)
-
-# I need this code to start the server on Heroku. Moved it to the bottom per Eric's suggestion. 
-
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))

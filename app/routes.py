4c1d3fab0034f85073aef6f7b2795f666c067691
Trey <thow123@gmail.com>
2018-03-15 21:15:18 -0700

    Moved debugger code
    Moved the code to turn on debugging and start the web server on heroku out of routes.py and into __init__.py

--- app/routes.py
+++ app/routes.py
@@ -268,12 +268,3 @@ def list_boards():
 # Pass the results into the template
 
    return render_template("boards.html",rows = boards_for_template)
-
-if __name__ == '__main__':
-   app.run(debug = True, use_reloader=True)
-
-# I need this code to start the server on Heroku. Moved it to the bottom per Eric's suggestion. 
-
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))

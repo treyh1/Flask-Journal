963265f1030fba30e01b36c42f90e3a8fc1d2257
Trey <thow123@gmail.com>
2018-03-24 11:54:32 -0700

    Added a Templates Fodler
    Specifying a templates folder for the Flask app.

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 

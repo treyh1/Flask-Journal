495538d3e4d44e5309946b38bf89e60f35e6c365
TH <thow123@gmail.com>
2019-06-14 08:11:44 -0700

    Added user class
    User class added to models.py

--- app/models.py
+++ app/models.py
@@ -65,3 +65,15 @@ class Beach(db.Model):
         self.lat = lat
         self.long = long
         self.beach_description = beach_description
+
+class Users(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, username, password_hash):
+        self.username = username
+        self.password_hash = password_hash

340f844b79b131a1f9529ff84c13d00d72b7e8d2
TH <thow123@gmail.com>
2019-06-20 17:29:34 -0700

    Remove index=True

--- app/models.py
+++ app/models.py
@@ -71,7 +71,7 @@ class Users(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
+    username = db.Column(db.String(64), unique=True)
     password_hash = db.Column(db.String(128))
 
     def __init__(self, username, password_hash):

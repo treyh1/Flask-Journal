3d13ea3142671848375fca6cff70feb99a0fef3a
Trey <thow123@gmail.com>
2019-07-13 14:52:15 -0700

    Revert "check_password_hash"
    This reverts commit ca6ab3319f49c3c70e588f4781edbe44b32699e7.

--- app/routes.py
+++ app/routes.py
@@ -366,7 +366,7 @@ def signin():
   form = LoginForm()
   if form.validate_on_submit():
     user = User.query.filter_by(username=form.username.data).first()
-    if user is None or not user.check_password_hash(form.password.data):
+    if user is None or not user.check_password(form.password.data):
       flash('Invalid username or password')
       return redirect(url_for('signin'))
     login_user(user, remember=form.remember_me.data)

4a1f57b49af1282148bd3eab2c4d897ca77d864a
TH <thow123@gmail.com>
2019-07-23 06:54:47 +0100

    Update __init__.py
    rearranged the order of the lines in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from app import models, routes
 
 app = Flask(__name__, template_folder="templates")
 app.config.from_object(Config)
+
+from app import models, routes
+
 db = SQLAlchemy(app)
 login = LoginManager(app)
 

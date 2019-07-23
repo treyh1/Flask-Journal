5f372a5eeebf3f1e8f66b43301ee53b73e7ed050
TH <thow123@gmail.com>
2019-07-23 06:56:57 +0100

    Revert "Update __init__.py"
    This reverts commit 4a1f57b49af1282148bd3eab2c4d897ca77d864a.

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,10 @@ from flask import Flask
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from app import models, routes
 
 app = Flask(__name__, template_folder="templates")
 app.config.from_object(Config)
-
-from app import models, routes
-
 db = SQLAlchemy(app)
 login = LoginManager(app)
 
